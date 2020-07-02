0bc9938677c0301363ac971e163ee7993d5c2ce6
Jerrypoi <jerry.poi.sz@gmail.com>
2020-07-02 17:47:00 +0800

    new help

--- builtins.c
+++ builtins.c
@@ -13,17 +13,15 @@ int help(char **args) {
     printf("%-10s  %s\n", "help", "输出帮助");
     printf("%-10s  %s\n", "exit", "退出系统");
     printf("%-10s  %s\n", "init", "初始化系统，挂载磁盘");
-    printf("%-10s  %s\n", "pwd", "输出当前路径");
+    printf("%-10s  %s\n", "format", "格式化文件系统为 FAT");
     printf("%-10s  %s\n", "ls", "显示当前路径中全部文件");
-    printf("%-10s  %s\n", "fs_mkdir", "make directories");
-    printf("%-10s  %s\n", "cd", "change working directory");
-    printf("%-10s  %s\n", "create", "create new file");
-    printf("%-10s  %s\n", "rm", "remove files");
-    printf("%-10s  %s\n", "rmdir", "remove directory");
-    printf("%-10s  %s\n", "open", "open a file");
-    printf("%-10s  %s\n", "close", "close a file");
-    printf("%-10s  %s\n", "write", "write_data to fd");
-    printf("%-10s  %s\n", "read", "read from fd");
+    printf("%-10s  %s\n", "mkdir dirname", "创建目录");
+    printf("%-10s  %s\n", "cd dirname", "前往目录");
+    printf("%-10s  %s\n", "touch filename", "创建新的文件");
+    printf("%-10s  %s\n", "write filename [a|w]", "写文件，write filename w 代表清空文件中所有数据，从头写入，write filename a 代表以附加的方式写入文件");
+    printf("%-10s  %s\n", "rmdir filename", "删除目录，目录必须为空");
+    printf("%-10s  %s\n", "cat filename", "输出文件内容");
+    printf("%-10s  %s\n", "chname old new", "将 old 文件/文件夹的名字改为 new");
     return true;
 }
 
